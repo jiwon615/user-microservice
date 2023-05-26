d83f7721f59dbc1ab63404065cc851eedc9a476e
jiwon615 <seg615@naver.com>
2023-05-26 20:21:16 +0900

    chore: project setting with libraries

---
+++ src/test/java/com/jimart/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimart.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
