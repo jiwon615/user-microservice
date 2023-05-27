268b8d83547e851e1dabb35f9e880e358c3a8551
jiwon615 <seg615@naver.com>
2023-05-27 19:22:09 +0900

    refacor: 공통 에러 핸들러 printStackTrace

--- src/main/java/com/jimart/userservice/core/exception/CommonExceptionAdvice.java
+++ src/main/java/com/jimart/userservice/core/exception/CommonExceptionAdvice.java
@@ -17,6 +17,7 @@ public class CommonExceptionAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ApiResponse<Object> bindException(BindException e) {
+        e.printStackTrace();
         return ApiResponse.ofError(
                 HttpStatus.BAD_REQUEST,
                 e.getBindingResult().getAllErrors().get(0).getDefaultMessage()
@@ -25,6 +26,7 @@ public class CommonExceptionAdvice {
 
     @ExceptionHandler(value = {CustomException.class})
     public ApiResponse<Object> customException(CustomException e) {
+        e.printStackTrace();
         return ApiResponse.ofError(
                 e.getErrorMsgType().getHttpStatus(),
                 e.getMessage()
@@ -33,9 +35,10 @@ public class CommonExceptionAdvice {
 
     @ExceptionHandler(value = {Exception.class})
     public ApiResponse<Object> commonException(Exception e) {
+        e.printStackTrace();
         return ApiResponse.ofError(
                 COMMON_SERVER_ERROR.getHttpStatus(),
-                COMMON_SERVER_ERROR.getMessage()
+                String.format("%s (%s)", COMMON_SERVER_ERROR.getMessage(), e.getLocalizedMessage())
         );
     }
 }
