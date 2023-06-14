1b4220d6b7289b56e5ea90e012294284a44df1d2
jiwon615 <seg615@naver.com>
2023-06-14 21:09:15 +0900

    rename: 클래스파일명 변경

--- src/main/java/com/jimart/userservice/core/config/FeignLogConfig.java
+++ src/main/java/com/jimart/userservice/core/config/FeignLogConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LoggerConfig {
+public class FeignLogConfig {
 
     @Bean
     public Logger.Level feignLoggerLevel() {
